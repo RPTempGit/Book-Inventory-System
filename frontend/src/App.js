55c86bd380c17b004a04ed76f09e4d8610f9f1fe
RPTempGit <Lucusho61@Gmail.com>
2026-01-07 23:25:38 +0800

    Add ResourceDashboard component below Navbar in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { AuthContextProvider } from "./context/AuthContext"
-import { TransactionContextProvider } from "./context/TransactionContext"
-import { StockTakeContextProvider } from "./context/StockTakeContext"
-import { useAuthContext } from "./hooks/useAuthContext"
-
-import Navbar from "./components/Navbar"
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import TransactionPage from "./pages/TransactionPage"
-import StockTakePage from "./pages/StockTakePage"
-import ReportsPage from "./pages/ReportsPage"
-
-function AppContent() {
-  const { user } = useAuthContext()
-
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="p-4 max-w-6xl mx-auto">
-        <Routes>
-          <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-          <Route path="/transaction" element={user ? <TransactionPage /> : <Navigate to="/login" />} />
-          <Route path="/stocktake" element={user ? <StockTakePage /> : <Navigate to="/login" />} />
-          <Route path="/reports" element={user ? <ReportsPage /> : <Navigate to="/login" />} />
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-          <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  )
-}
-
-/*Hello, Jeron*/
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ResourceDashboard from './components/ResourceDashboard';
+import HomePage from './pages/HomePage';
+import './App.css';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <TransactionContextProvider>
-        <StockTakeContextProvider>
-          <AppContent />
-        </StockTakeContextProvider>
-      </TransactionContextProvider>
-    </AuthContextProvider>
-  )
+    <Router>
+      <Navbar />
+      <ResourceDashboard />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
